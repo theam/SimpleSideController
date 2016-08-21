5cab5e34d25afe379bb4722c600be5d94cfb30b8
Alessandro Martin <alessandro@theagilemonkeys.com>
2016-08-21 21:38:21 +0100

    Initial Commit

---
+++ SimpleSideController/SimpleSideController.h
@@ -0,0 +1,19 @@
+//
+//  SimpleSideController.h
+//  SimpleSideController
+//
+//  Created by Alessandro Martin on 21/08/16.
+//  Copyright © 2016 Alessandro Martin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SimpleSideController.
+FOUNDATION_EXPORT double SimpleSideControllerVersionNumber;
+
+//! Project version string for SimpleSideController.
+FOUNDATION_EXPORT const unsigned char SimpleSideControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleSideController/PublicHeader.h>
+
+
